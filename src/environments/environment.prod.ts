35eee3418f456ecaebf512d630b40b627d937d5b
Lorenzo Sepulveda <lorenzosepulveda1@gmail.com>
2023-05-19 20:10:16 -0300

    enviroments change commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrlEdu: 'hhttps://backendls.onrender.com/educacion/',
+  URL: 'https://backendls.onrender.com/',
+  apiUrlEdu: 'https://backendls.onrender.com/educacion/',
   apiUrlPer: 'https://backendls.onrender.com/personas/',
   apiUrlExp: 'https://backendls.onrender.com/explab/',
   authUrl: 'https://backendls.onrender.com/auth/',
